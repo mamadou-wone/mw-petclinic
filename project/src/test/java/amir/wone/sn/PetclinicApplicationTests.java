d5b81d3132a8570c9cb6604b523872b66116ca29
Mamadou WONE <wone.dev@gmail.com>
2021-08-03 15:27:36 +0000

    Initial Project

---
+++ project/src/test/java/amir/wone/sn/PetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package amir.wone.sn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
